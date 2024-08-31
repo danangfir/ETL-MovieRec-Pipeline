14c311cacdd41f91c65d2a02cc486a785f530bcb
DGFR <100404424+danangfir@users.noreply.github.com>
2024-08-31 09:44:18 +0700

    changes main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from src.load import create_database, create_table, insert_movies_data, insert_m
 from src.recommend import recommend_movies
 
 def main():
-    # Step 1: Extract
     """
     Main entry point for MovieLens ETL and recommendation system.
 
@@ -12,23 +11,24 @@ def main():
     the required format, loads the data into a SQLite database, and provides a
     CLI for recommending movies based on a given movie ID.
     """
+    # Extract
     movies_df = load_movies_data('data/movies.csv')
     ratings_df = load_ratings_data('data/ratings.csv')
 
-    # Step 2: Transform
+    # Transform
     movies_df = clean_movie_titles(movies_df)
     movies_df = split_genres(movies_df)
     avg_ratings_df = calculate_average_ratings(ratings_df)
     movies_df = movies_df.merge(avg_ratings_df, on='movie_id')
     movies_df = create_movie_features(movies_df)
 
-    # Step 3: Load
+    # Load
     conn = create_database('movies.db')
-    create_tables(conn)
+    create_table(conn)
     insert_movies_data(conn, movies_df)
     insert_movie_features_data(conn, movies_df)
 
-    # Step 4: Recommendation (CLI)
+    # Recommendation (CLI)
     while True:
         movie_id = int(input("Enter a movie ID to get recommendations: "))
         recommendations = recommend_movies(conn, movie_id)
